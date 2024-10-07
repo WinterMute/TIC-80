e33f5fab3ce4b82465a526b8617a7cb6991482f1
Miguel <81558772+Miguel-hrvs@users.noreply.github.com>
2024-10-07 11:02:15 +0200

    fix

--- src/api/mruby.c
+++ src/api/mruby.c
@@ -1051,12 +1051,12 @@ static mrb_bool catcherr(tic_core* core)
     {
         mrb_value ex = mrb_obj_value(mrb->exc);
 
-        mrb_print_backtrace(mrb, mrb->exc);
+        mrb_print_backtrace(mrb);
         mrb_value insp = mrb_inspect(mrb, ex);
 
         char* cmsg = mrb_str_to_cstr(mrb, insp);
         core->data->error(core->data->data, cmsg);
-        
+
         mrb->exc = NULL;
 
         return false;
